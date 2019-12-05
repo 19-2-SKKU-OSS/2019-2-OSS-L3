cd3bfbaa0fde86756b4106348c9aed1aee811e80
CHINWH <56297600+CHINWH@users.noreply.github.com>
2019-12-05 19:23:33 +0900

    Update Fibonacci.c

--- misc/Fibonacci.c
+++ misc/Fibonacci.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-//Fibonnacci function 
+//Fibonnacci function introduce 
+
 int fib(int number){
 	if(number==1||number==2) return 1;
 	else return fib(number-1)+fib(number-2);
