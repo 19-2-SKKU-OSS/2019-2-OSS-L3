954d0382e137b049e008625571d309ba478d2030
mii2026 <50134759+mii2026@users.noreply.github.com>
2019-12-07 23:52:49 +0900

    Create multiply_with_3.5.c

---
+++ bit_algorithm/multiply_with_3.5.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+// multiply a number with 3.5 
+int multiplyWith3Point5(int x) 
+{ 
+  return ((x<<3) - x)>>1; 
+}  
+
+// Driver program to test above functions
+int main() 
+{ 
+    int x = 4;  
+    printf("%d", multiplyWith3Point5(x)); 
+    getchar(); 
+    return 0; 
+}
