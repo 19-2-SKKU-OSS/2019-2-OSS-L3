aa3701b77329425c2d69487ad7b7f8c915da0dd5
mii2026 <50134759+mii2026@users.noreply.github.com>
2019-12-07 15:29:19 +0900

    [CREATE] create boolean_array_puzzle.c

---
+++ bit_algorithm/boolean_array_puzzle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void changeToZero(int a[2]) 
+{ 
+   a[ a[1] ] = a[ !a[1] ]; 
+} 
+  
+int main() 
+{ 
+   int a[] = {1, 0}; 
+   changeToZero(a); 
+  
+   printf(" arr[0] = %d \n", a[0]); 
+   printf(" arr[1] = %d ", a[1]); 
+   getchar(); 
+   return 0; 
+} 
